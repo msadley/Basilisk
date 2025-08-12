972a151f2bb28af25b0cc4fad47b280a14117a3c
msadley <mendes.adley@gmail.com>
2025-08-11 22:17:26 -0300

    Added logging mechanism

---
+++ src/util/log.ts
@@ -0,0 +1,29 @@
+// src/util/log.ts
+
+import { absolutePath } from "./util.js";
+import fs from "fs";
+
+const LOG_FILE = "/log/log.txt";
+
+function getCurrentTimestamp() : string {
+  const now = new Date();
+
+  const pad = (num: number): string => num.toString().padStart(2, "0");
+
+  const year = now.getFullYear();
+  const month = pad(now.getMonth() + 1);
+  const day = pad(now.getDate());
+  const hours = pad(now.getHours());
+  const minutes = pad(now.getMinutes());
+  const seconds = pad(now.getSeconds());
+  const milliseconds = now.getMilliseconds().toString().padStart(3, "0");
+
+  return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}.${milliseconds}`;
+};
+
+export async function log(level: "INFO" | "WARN" | "ERROR", message: string) {
+  await fs.promises.appendFile(
+    absolutePath(LOG_FILE),
+    `[${getCurrentTimestamp}] [${level}] ${message}`
+  );
+}
