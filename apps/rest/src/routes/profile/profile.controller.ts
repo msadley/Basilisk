8438e3c4f62edbec0e1e1de58ba380437e833ec8
msadley <mendes.adley@gmail.com>
2025-10-17 00:05:58 -0300

    Added zod to validate user input

--- apps/rest/src/routes/profile/profile.controller.ts
+++ apps/rest/src/routes/profile/profile.controller.ts
@@ -2,22 +2,36 @@
 
 import { type Request, type Response } from "express";
 import { basilisk } from "../../index.js";
+import { z } from "zod";
 
 export const getProfile = async (_req: Request, res: Response) => {
   res.status(200).json(await basilisk.getProfile());
 };
 
+const updateProfileSchema = z.object({
+  name: z.string().optional(),
+  profilePicture: z.url().optional(),
+});
+
 export const setProfile = async (req: Request, res: Response) => {
-  const { name, profilePicture } = req.body;
+  try {
+    const { name, profilePicture } = updateProfileSchema.parse(req.body);
 
-  if (name) {
-    await basilisk.setName(name);
+    if (name) {
+      await basilisk.setName(name);
+    }
+
+    if (profilePicture) {
+      await basilisk.setProfilePicture(profilePicture);
+    }
+
+    const updatedProfile = await basilisk.getProfile();
+    res.status(200).json(updatedProfile);
+  } catch (error) {
+    if (error instanceof z.ZodError) {
+      res.status(400).json({ error: error.message });
+    } else {
+      throw error;
+    }
   }
-
-  if (profilePicture) {
-    await basilisk.setProfilePicture(profilePicture);
-  }
-
-  const updatedProfile = await basilisk.getProfile();
-  res.status(200).json(updatedProfile);
 };
