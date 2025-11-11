1866677ae7854d06ab879a0fe834c845bdf3de03
msadley <mendes.adley@gmail.com>
2025-11-11 17:40:14 -0300

    Improved Home view

--- apps/web/src/components/MainArea/views/Home/Home.tsx
+++ apps/web/src/components/MainArea/views/Home/Home.tsx
@@ -1,37 +1,52 @@
 import styles from "./Home.module.css";
 import type { ViewProps } from "../../../../types";
 import { motion } from "framer-motion";
-import { useEffect } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { userStore } from "../../../../stores/UserStore";
+import { observer } from "mobx-react-lite";
+import { Icon } from "@iconify/react";
 
-function Home({
-  setHeader,
-  setFooter,
-  setLeftPanel,
-  setRightPanel,
-}: ViewProps) {
-  const profile = userStore.userProfile;
+const Home = observer(
+  ({ setHeader, setFooter, setLeftPanel, setRightPanel }: ViewProps) => {
+    const [idVisible, setIdVisible] = useState<boolean>(false);
+    const profile = userStore.userProfile;
 
-  useEffect(() => {
-    setHeader(<></>);
-    setFooter(<></>);
-    setLeftPanel(<></>);
-    setRightPanel(<></>);
-  }, [setHeader, setFooter, setLeftPanel, setRightPanel]);
+    const toggleIdVisibility = useCallback(() => {
+      setIdVisible((prev) => !prev);
+    }, []);
 
-  return (
-    <motion.div
-      initial={{ opacity: 0 }}
-      animate={{ opacity: 1 }}
-      exit={{ opacity: 0 }}
-      transition={{ duration: 0.2 }}
-      className={styles.home}
-    >
-      <img src="/basilisk.svg" alt="logo" />
-      <p>Bem-vindo ao Basilisk</p>
-      <p>Seu ID de Usuário: {profile?.id}</p>
-    </motion.div>
-  );
-}
+    const copyIdToClipboard = useCallback(() => {
+      const data = profile?.id ?? "";
+      navigator.clipboard.writeText(data);
+    }, []);
+
+    useEffect(() => {
+      setHeader(<></>);
+      setFooter(<></>);
+      setLeftPanel(<></>);
+      setRightPanel(<></>);
+    }, [setHeader, setFooter, setLeftPanel, setRightPanel]);
+
+    return (
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        exit={{ opacity: 0 }}
+        transition={{ duration: 0.2 }}
+        className={styles.home}
+      >
+        <img src="/basilisk.svg" alt="logo" />
+        <div className={styles.idWrapper}>
+          <button onClick={toggleIdVisibility}>
+            {idVisible ? profile?.id : "Mostrar ID"}
+          </button>
+          <button onClick={copyIdToClipboard} className={styles.copyButton}>
+            <Icon icon="mingcute:copy-2-fill"></Icon>
+          </button>
+        </div>
+      </motion.div>
+    );
+  }
+);
 
 export default Home;
