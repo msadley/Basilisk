c11c5c4bff64c68e2f0c2d740c94ad8f32738ccf
msadley <mendes.adley@gmail.com>
2025-11-11 17:43:19 -0300

    Added more event passing through to the rootStore

--- apps/web/src/stores/RootStore.ts
+++ apps/web/src/stores/RootStore.ts
@@ -5,6 +5,9 @@ class RootStore {
   constructor() {
     makeAutoObservable(this);
     workerController.on("node-started", this.loadSequence);
+    workerController.on("message-received", this.messageReceive);
+    workerController.on("relay-lost", this.setIndicatorOff);
+    workerController.on("relay-found", this.setIndicatorOn);
   }
 
   loadSequence = async () => {
@@ -12,6 +15,24 @@ class RootStore {
     const { userStore } = await import("./UserStore");
 
     await Promise.all([userStore.initialLoad(), chatStore.initialLoad()]);
+  };
+
+  messageReceive = async (event: any) => {
+    const { messageStore } = await import("./MessageStore");
+
+    await messageStore.handleMessageReceived(event.message);
+  };
+
+  setIndicatorOff = async () => {
+    const { connectionStore } = await import("./ConnectionStore");
+
+    connectionStore.setConnectionFalse();
+  };
+
+  setIndicatorOn = async () => {
+    const { connectionStore } = await import("./ConnectionStore");
+
+    connectionStore.setConnectionTrue();
   };
 }
 
