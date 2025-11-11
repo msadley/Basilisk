6a0a652c432fb41d244f02d1cc380b581dddff4d
msadley <mendes.adley@gmail.com>
2025-11-11 17:39:32 -0300

    Improved input box

--- apps/web/src/components/MainArea/views/Chat/InputBox/InputBox.tsx
+++ apps/web/src/components/MainArea/views/Chat/InputBox/InputBox.tsx
@@ -1,20 +1,39 @@
+import { useEffect } from "react";
 import styles from "./InputBox.module.css";
 import { useForm, type SubmitHandler } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import z from "zod";
 
-type Inputs = {
-  message: string;
-};
+const formSchema = z.object({
+  messageText: z.string().trim().nonempty(),
+});
+
+type Inputs = z.infer<typeof formSchema>;
 
 type InputBoxProps = {
   sendMessage: (message: string) => void;
 };
 
 const InputBox = ({ sendMessage }: InputBoxProps) => {
-  const { register, handleSubmit } = useForm<Inputs>();
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { isSubmitSuccessful },
+  } = useForm<Inputs>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      messageText: "",
+    },
+  });
 
   const onSubmit: SubmitHandler<Inputs> = async (data) => {
-    sendMessage(data.message);
+    sendMessage(data.messageText);
   };
+
+  useEffect(() => {
+    if (isSubmitSuccessful) reset();
+  }, [isSubmitSuccessful, reset]);
 
   return (
     <div className={styles.inputBox}>
@@ -25,7 +44,7 @@ const InputBox = ({ sendMessage }: InputBoxProps) => {
         <input
           type="text"
           placeholder="Digite uma mensagem..."
-          {...register("message")}
+          {...register("messageText")}
         />
         <button type="submit">Enviar</button>
       </form>
