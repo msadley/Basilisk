8bc2cc0324280da26dcc69c9afaf5cfd28dac93c
msadley <mendes.adley@gmail.com>
2025-11-17 22:20:40 -0300

    Added pm2 reservation metrics to relay

--- apps/relay/src/relay.ts
+++ apps/relay/src/relay.ts
@@ -3,12 +3,21 @@
 import "dotenv/config";
 import { type Multiaddr } from "@multiformats/multiaddr";
 import { Node } from "@basilisk/core";
+import io from "@pm2/io";
+
+import { type CircuitRelayService } from "@libp2p/circuit-relay-v2";
 
 let basilisk: Node;
+
+let reservationPoller: NodeJS.Timeout;
 
 const PUBLIC_DNS: string | undefined = process.env.PUBLIC_DNS;
 
 if (!PUBLIC_DNS) throw new Error("No public dns was specified.");
+
+const relayReservations = io.metric({
+  name: "Relay Reservations",
+});
 
 export async function start() {
   basilisk = await Node.init({
@@ -17,6 +26,22 @@ export async function start() {
   });
 
   printAddresses();
+
+  reservationPoller = setInterval(() => {
+    if (!basilisk || !basilisk.node) {
+      return;
+    }
+
+    const relayService = basilisk.node.services
+      .circuitRelay as CircuitRelayService;
+
+    if (relayService) {
+      const reservationCount = relayService.reservations.size;
+      relayReservations.set(reservationCount);
+    } else {
+      relayReservations.set(0);
+    }
+  }, 2000);
 
   process.on("SIGINT", async () => {
     await stop();
@@ -30,6 +55,9 @@ export async function start() {
 }
 
 async function stop() {
+  if (reservationPoller) {
+    clearInterval(reservationPoller);
+  }
   if (basilisk) {
     await basilisk.stop();
   }
