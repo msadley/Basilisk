0a44fb72e1d09086de3dca885fdbaa6280141924
msadley <mendes.adley@gmail.com>
2026-02-03 20:58:26 -0300

    Simplified chat header

--- apps/web/src/components/MainArea/views/Chat/Header/Header.tsx
+++ apps/web/src/components/MainArea/views/Chat/Header/Header.tsx
@@ -3,8 +3,7 @@ import styles from "./Header.module.css";
 const Header = () => {
   return (
     <div className={styles.wrapper}>
-      <div className={styles.nebulaOrb}></div>
-      <div className={styles.msg}>A origem...</div>
+      <div className={styles.bar}></div>
     </div>
   );
 };
